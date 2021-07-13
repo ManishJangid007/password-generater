5ceb74516c23e96115adf210b90b0c0eef28c769
Manish <anitasharma4440@gmail.com>
2021-07-12 21:28:52 -0700

    "first commit"

---
+++ passwordGenerator.py
@@ -0,0 +1,46 @@
+import random
+from tkinter import *
+
+root = Tk()
+root.title("Password Generator")
+
+
+def generate_password():
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    num = "0123456789"
+    symbols = "@#&/"
+
+    all_char = str(lower) + str(upper) + str(symbols) + str(num)
+
+    password = "".join(random.sample(all_char, length_value))
+
+    return password
+
+
+def my_password():
+    if 3 <= length_value <= 50:
+        password = Label(root, text=f"Password : {generate_password()}")
+        password.pack()
+        # print(f"Password : {generatepassword()}")
+    elif length_value < 3:
+        error_1 = Label(root, text="Length is too short ! try again")
+        error_1.pack()
+        # print("Length is too short ! try again")
+    elif length_value > 20:
+        error_2 = Label(root, text="Length is higher than expected ! try again")
+        error_2.pack()
+        # print("Length is higher than expected ! try again")
+
+if __name__ == '__main__':
+    global length_value
+    length = Entry(root)
+    length.pack()
+    length_value = int(length.get())
+    generate_button = Button(root, text="Generate Password", command=my_password)
+    generate_button.pack()
+    # length = int(input("Enter the length of password : "))
+
+
+root.mainloop()
+
